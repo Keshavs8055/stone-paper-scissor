f3248f01e98cc4fa835bdaa817441b6b1453de77
Keshav Sharma <Keshavs8055@gmail.com>
2025-02-22 16:14:44 +0530

    Added Docker

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Game from "./components/game/game.comp";
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="main">
-          <h1>Rock, Paper, Scissor</h1>
-          <h5>By Keshav Sharma </h5>
-        </div>
-        <Game />
+const App = () => {
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>Rock, Paper, Scissor</h1>
+        <h5>By Keshav Sharma </h5>
       </div>
-    );
-  }
-}
+      <Game />
+    </div>
+  );
+};
 
 export default App;
